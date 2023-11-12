7d5f7d36f6d94807fe53c6099b7ae38fec6bdcb8
chowdarychandra <chandrahari321@gmail.com>
2023-11-12 20:48:40 +0530

    update

--- expense-shell/backend.sh
+++ expense-shell/backend.sh
@@ -16,4 +16,4 @@ systemctl enable backend
 systemctl start backend
 
 dnf install mysql -y
-mysql -h mysql.rdevopsb72.online -uroot -pExpenseApp@1 < /app/schema/backend.sql
+mysql -h mysql.roboshop.internal -uroot -pExpenseApp@1 < /app/schema/backend.sql
