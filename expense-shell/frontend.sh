18c67ac5cd8550438d31c8be3c6a42685f628ba3
chowdarychandra <chandrahari321@gmail.com>
2023-11-12 21:59:17 +0530

    update

--- expense-shell/frontend.sh
+++ expense-shell/frontend.sh
@@ -1,15 +1,21 @@
-dnf install nginx -y
-cp expense.conf /etc/nginx/default.d/expense.conf
+echo installing nginx
+dnf install nginx -y >>/tmp/nginx
 
+echo placlling expense configuration file
+cp expense.conf /etc/nginx/default.d/expense.conf >>/tmp/nginx
 
+echo removing onld nginx web content
+rm -rf /usr/share/nginx/html/* >>/tmp/nginx
+
+echo downloading new nginx web content
+curl -o /tmp/frontend.zip https://expense-artifacts.s3.amazonaws.com/frontend.zip >>/tmp/nginx
+
+echo chinging nginx directory
+cd /usr/share/nginx/html >>/tmp/nginx
+
+echo extracting frontend code
+unzip /tmp/frontend.zip  >>/tmp/nginx
+
+echo starting nginx server
 systemctl enable nginx
-systemctl start nginx
-rm -rf /usr/share/nginx/html/*
-
-
-curl -o /tmp/frontend.zip https://expense-artifacts.s3.amazonaws.com/frontend.zip
-
-cd /usr/share/nginx/html
-unzip /tmp/frontend.zip
-
 systemctl restart nginx
