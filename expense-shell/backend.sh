85b814a56a15c68e594cbc2d5d84ff22669cf781
chowdarychandra <chandrahari321@gmail.com>
2023-11-12 20:48:00 +0530

    update

--- expense-shell/backend.sh
+++ expense-shell/backend.sh
@@ -1,31 +1,19 @@
 curl -sL https://rpm.nodesource.com/setup_lts.x | bash
 
 dnf install nodejs -y
-
-echo Copy Backend Service File
 cp backend.service /etc/systemd/system/backend.service
 
-echo Add Application User
 useradd expense
-
-
-rm -rf /app # to remove old files
-
-echo Copy Backend Service File
+mkdir /app
 curl -o /tmp/backend.zip https://expense-artifacts.s3.amazonaws.com/backend.zip
-
+cd /app
 unzip /tmp/backend.zip
 
 npm install
 
-echo Start Backend Service
 systemctl daemon-reload
-
 systemctl enable backend
-
 systemctl start backend
 
-
-
 dnf install mysql -y
-mysql -h mysql.roboshop.internal -uroot -pExpenseApp@1 < /app/schema/backend.sql
+mysql -h mysql.rdevopsb72.online -uroot -pExpenseApp@1 < /app/schema/backend.sql
--- expense-shell/frontend.sh
+++ expense-shell/frontend.sh
@@ -1,20 +1,15 @@
-echo Installing Nginx
-dnf install nginx -y 
+dnf install nginx -y
+cp expense.conf /etc/nginx/default.d/expense.conf
 
-echo Placing Expense Config File in Nginx
-cp expense.conf /etc/nginx/default.d/expense.conf 
 
-echo Removing Old Nginx content
-rm -rf /usr/share/nginx/html/* 
+systemctl enable nginx
+systemctl start nginx
+rm -rf /usr/share/nginx/html/*
 
-echo Download Frontend Code
-curl -o /tmp/frontend.zip https://expense-artifacts.s3.amazonaws.com/frontend.zip 
+
+curl -o /tmp/frontend.zip https://expense-artifacts.s3.amazonaws.com/frontend.zip
 
 cd /usr/share/nginx/html
+unzip /tmp/frontend.zip
 
-echo Extracting Frontend Code
-unzip /tmp/frontend.zip 
-
-echo Starting Nginx Service
-systemctl enable nginx 
-systemctl restart nginx 
+systemctl restart nginx
