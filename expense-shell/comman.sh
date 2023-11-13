c1b927d68c13d2160855fbe1b2c1bc0d2b36f021
chowdarychandra <chandrahari321@gmail.com>
2023-11-13 17:38:36 +0530

    update

--- expense-shell/comman.sh
+++ expense-shell/comman.sh
@@ -4,6 +4,7 @@ log_file=/tmp/expense.log
 download_and_extract() {
  echo downloading $component code
  curl -o /tmp/$component.zip https://expense-artifacts.s3.amazonaws.com/$component.zip &>>$log_file
+ 
  if [ $? -eq 0 ]; then
  echo SUCCESS
 else
@@ -12,6 +13,7 @@ fi
 
  echo extracting $component code
  unzip /tmp/$component.zip  &>>$log_file
+ 
  if [ $? -eq 0 ]; then
  echo SUCCESS
 else
