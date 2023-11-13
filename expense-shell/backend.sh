538e1fabe9e53043fe1fc1cb4b266d3cd9205db0
chowdarychandra <chandrahari321@gmail.com>
2023-11-13 10:16:38 +0530

    update

--- expense-shell/backend.sh
+++ expense-shell/backend.sh
@@ -17,17 +17,25 @@ rm -rf /app
 
 mkdir /app
 
-
+echo download app content
 curl -o /tmp/backend.zip https://expense-artifacts.s3.amazonaws.com/backend.zip >>$log_file
+
+
 cd /app
 
+echo export app content
 unzip /tmp/backend.zip >>$log_file
 
+echo downloas dependencies
 npm install >>$log_file
 
+echo start backend services
 systemctl daemon-reload >>$log_file
-systemctl enable backend
-systemctl start backend
+systemctl enable backend >>$log_file
+systemctl start backend >>$log_file
 
+echo install mysql client
 dnf install mysql -y >>$log_file
+
+echo load the mysql schema
 mysql -h mysql.roboshop.internal -uroot -pExpenseApp@1 < /app/schema/backend.sql >>$log_file
--- expense-shell/mysql.sh
+++ expense-shell/mysql.sh
@@ -1,9 +1,15 @@
+echo disable mysql 8 vershion
 dnf module disable mysql -y
 
+echo copy mysql repo file
 cp mysql.repo /etc/yum.repos.d/mysql.repo
+
+echo install mysql server
 dnf install mysql-community-server -y
 
+echo start mysql server
 systemctl enable mysqld
 systemctl start mysqld
 
+echo setup root password
 mysql_secure_installation --set-root-pass ExpenseApp@1
