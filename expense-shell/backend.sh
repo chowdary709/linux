b2f74cd9c7ebec33d67c6c7b3a79fb92a553ce2a
chowdarychandra <chandrahari321@gmail.com>
2023-11-13 11:35:13 +0530

    update

--- expense-shell/backend.sh
+++ expense-shell/backend.sh
@@ -1,40 +1,37 @@
 source comman.sh
+component=backend
 
 echo install nodejs repos
-curl -sL https://rpm.nodesource.com/setup_lts.x | bash >>$log_file
+curl -sL https://rpm.nodesource.com/setup_lts.x | bash &>>$log_file
 
 echo installing nodejs
-dnf install nodejs -y >>$log_file
+dnf install nodejs -y &>>$log_file
 
 echo copy backend service file
-cp backend.service /etc/systemd/system/backend.service >>$log_file
+cp backend.service /etc/systemd/system/backend.service &>>$log_file
 
 echo add aplication user
-useradd expense >>$log_file
+useradd expense &>>$log_file
 
 echo cleen app content
 rm -rf /app
 
 mkdir /app
 
-echo download app content
-curl -o /tmp/backend.zip https://expense-artifacts.s3.amazonaws.com/backend.zip >>$log_file
-
 cd /app
 
-echo export app content
-unzip /tmp/backend.zip >>$log_file
+download_and_extract
 
 echo downloas dependencies
-npm install >>$log_file
+npm install &>>$log_file
 
 echo start backend services
-systemctl daemon-reload >>$log_file
-systemctl enable backend >>$log_file
-systemctl start backend >>$log_file
+systemctl daemon-reload &>>$log_file
+systemctl enable backend &>>$log_file
+systemctl start backend &>>$log_file
 
 echo install mysql client
-dnf install mysql -y >>$log_file
+dnf install mysql -y &>>$log_file
 
 echo load the mysql schema
-mysql -h mysql.roboshop.internal -uroot -pExpenseApp@1 < /app/schema/backend.sql >>$log_file
+mysql -h mysql.roboshop.internal -uroot -pExpenseApp@1 < /app/schema/backend.sql &>>$log_file
--- expense-shell/frontend.sh
+++ expense-shell/frontend.sh
@@ -1,22 +1,19 @@
 source comman.sh
+component=frontend
 
 echo installing nginx
-dnf install nginx -y >>$log_file
+dnf install nginx -y &>>$log_file
 
 echo placlling expense configuration file
-cp expense.conf /etc/nginx/default.d/expense.conf >>$log_file
+cp expense.conf /etc/nginx/default.d/expense.conf &>>$log_file
 
 echo removing onld nginx web content
-rm -rf /usr/share/nginx/html/* >>$log_file
-
-echo downloading new nginx web content
-curl -o /tmp/frontend.zip https://expense-artifacts.s3.amazonaws.com/frontend.zip >>$log_file
+rm -rf /usr/share/nginx/html/* &>>$log_file
 
 echo chinging nginx directory
-cd /usr/share/nginx/html >>$log_file
+cd /usr/share/nginx/html &>>$log_file
 
-echo extracting frontend code
-unzip /tmp/frontend.zip  >>$log_file
+download_and_extract
 
 echo starting nginx server
 systemctl enable nginx
