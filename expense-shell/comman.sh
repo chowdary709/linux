ad60ab95783fc1f8de823853148bf46281282c73
chowdarychandra <chandrahari321@gmail.com>
2023-11-13 11:45:37 +0530

    update

--- expense-shell/comman.sh
+++ expense-shell/comman.sh
@@ -3,8 +3,8 @@ log_file=/tmp/expense.log
 
 download_and_extract() {
  echo downloading $component code
- curl -o /tmp/$component.zip https://expense-artifacts.s3.amazonaws.com/$component.zip &>>$log_file
+ curl -o /tmp/$component.zip https://expense-artifacts.s3.amazonaws.com/$component.zip &>$log_file
 
  echo extracting $component code
- unzip /tmp/$component.zip  &>>$log_file
+ unzip /tmp/$component.zip  &>$log_file
 }
--- expense-shell/frontend.sh
+++ expense-shell/frontend.sh
@@ -2,16 +2,16 @@ source comman.sh
 component=frontend
 
 echo installing nginx
-dnf install nginx -y &>>$log_file
+dnf install nginx -y &>$log_file
 
 echo placlling expense configuration file
-cp expense.conf /etc/nginx/default.d/expense.conf &>>$log_file
+cp expense.conf /etc/nginx/default.d/expense.conf &>$log_file
 
 echo removing onld nginx web content
-rm -rf /usr/share/nginx/html/* &>>$log_file
+rm -rf /usr/share/nginx/html/* &>$log_file
 
 echo chinging nginx directory
-cd /usr/share/nginx/html &>>$log_file
+cd /usr/share/nginx/html &>$log_file
 
 download_and_extract
 
